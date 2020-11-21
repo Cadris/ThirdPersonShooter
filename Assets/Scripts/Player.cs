307087a58f51667d51da0f9d376b6f7e00e4bf98
cadris <cadrissilver@gmail.com>
2020-11-21 21:23:01 +0530

    Episode 2

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Player : MonoBehaviour
+{  
+    [System.Serializable]
+    public class MouseInput {
+        public Vector2 Damping;
+        public Vector2 Sensitivity;
+    }
+
+    [SerializeField] float speed;
+    [SerializeField]MouseInput MouseControl;
+
+    InputController playerInput;
+    void Awake()
+    {
+        playerInput=GameManager.Instance.InputController;
+    }
+    
+    void Update()
+    {
+        Vector2 direction = new Vector2(playerInput.Vertical*speed, playerInput.Horizontal*speed);
+    }
+}
