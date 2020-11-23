9620e33a47b15bd280891b8860b09463e553a19a
cadris <cadrissilver@gmail.com>
2020-11-24 00:51:05 +0530

    Last Added Health Script. For Player Not Assigned Error delete and reattach the ThirdPersonCamera Script to the main camera

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : Destructable
+{
+    public override void Die()
+    {
+        base.Die();
+
+        print("We Died.");        
+    }
+
+    public override void TakenDamage(float amount)
+    {        
+        base.TakenDamage(amount);
+        print("Remaining: "+HitPointsRemaining);
+    }
+}
---
+++ Assets/Shared/Shooter.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+    [SerializeField]float rateOfFire;
+    [SerializeField]Projectile projectile;
+
+    [HideInInspector]
+    public Transform muzzle;
+
+    float nextFireAllowed;
+    public bool canFire;
+
+    void Awake() {
+        muzzle = transform.Find("Muzzle");
+    }
+
+    public virtual void Fire(){
+
+        print("Firing !");
+        canFire = false;
+
+        if(Time.time < nextFireAllowed)
+            return;
+        
+        nextFireAllowed = Time.time + rateOfFire;
+
+        //print("Firing ! : "+Time.time);
+        //Instantiate the Projectile
+        Instantiate(projectile, muzzle.position, muzzle.rotation);
+
+        canFire = true;
+    }
+}
